13f3b6a4ec9b0a2188629555b1b4f65554e908ea
Chris 'Arom' Ilkow <aromchris@hotmail.com>
2014-11-28 15:47:47 +0000

    Commiting Feeds.cs class

---
+++ CSAlumni/Feeds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAlumni
+{
+    class Feeds
+    {
+        public string alumni_email { get; set; }
+        public int twitter { get; set; }
+        public int facebook { get; set; }
+        public int rss { get; set; }
+        public int atom { get; set; }
+        public int email { get; set; }
+        
+        public Feeds(int twitter, int facebook, int rss, int atom, int email, string alumni_email)
+        {
+            this.twitter = twitter;
+            this.facebook = facebook;
+            this.rss = rss;
+            this.atom = atom;
+            this.email = email;
+            this.alumni_email = alumni_email;
+        }
+        public Feeds()
+        {
+
+        }
+
+    }
+}
